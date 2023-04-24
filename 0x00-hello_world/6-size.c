1ed45037ef237ec258692a91874e713b45eb4458
assiatahtaoui <tahtaouiassia04@gmail.com>
2023-04-24 01:59:43 -0700

    m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,41 +1,15 @@
-#include <stdlib.h>
-
-#include <time.h>
-
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - prints if number is positive, zero or negative
- * Return: Always (Success)
- *
- */
-int                                                main(void)
-
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-    int n;
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    if (n > 0) {
-
-        printf("%d is positive\n", n);
-    }
-
-    else if (n == 0)
-
-
-    {
-        printf("%d is zero\n", n);
-
-    } else if (n < 0)
-
-    {
-        printf("%d is negative\n", n);
-
-    }
-
-
-
-    return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
-
